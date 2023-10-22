46b9826cad017a6cb31c2e146e54fb7d33a511ae
lcroberts <logancroberts@outlook.com>
2023-10-22 16:31:31 -0400

    Initialize repo

---
+++ src/main/java/com/csc340/CrudApp/CrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.CrudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppApplication.class, args);
+	}
+
+}
