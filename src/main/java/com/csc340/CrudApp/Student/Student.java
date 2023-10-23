0d1add28257d86976a731a6d2cc4cec2c82bd113
Logan Roberts <logancroberts@outlook.com>
2023-10-23 14:28:46 -0400

    Update id generation type

--- src/main/java/com/csc340/CrudApp/Student/Student.java
+++ src/main/java/com/csc340/CrudApp/Student/Student.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Table(name = "student")
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
